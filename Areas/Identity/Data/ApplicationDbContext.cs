6353908aa720e206173c723551c725ff8796da6e
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2023-05-08 13:23:15 -0500

    Add project files.

---
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using SecureAssetManager.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SecureAssetManager.Models;
+
+namespace SecureAssetManager.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new AppUserEntityConfiguration());
+    }
+
+    public class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(p => p.FirstName).HasMaxLength(50);
+            builder.Property(p => p.LastName).HasMaxLength(50);
+        }
+    }
+
+    public DbSet<SecureAssetManager.Models.Post> Post { get; set; }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace SecureAssetManager.Models
+{
+    public class Post
+    {
+
+        public int ID { get; set; }
+        // Linked to identity user
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        // Initialize Date
+
+
+        [Required]
+        [
+            StringLength(
+                100,
+                ErrorMessage = "Descripción no puede estar vacía",
+                MinimumLength = 2)
+        ]
+        public string? Descripcion { get; set; }
+
+        public string? ImagenUrl { get; set; }
+        // Image type
+        [Display(Name = "Subir una imagen ")]
+        [NotMapped]
+        public IFormFile? ImagenFile { get; set; }
+
+    }
+}
